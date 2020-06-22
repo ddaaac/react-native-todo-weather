ac6975ffb187d4328a2f198fe288c84fda4010d1
BEOMJUN KIM <skyblue300a@gmail.com>
2020-06-22 11:29:03 +0900

    feat: TodoInput으로 TodoItem을 추가하는 기능 구현

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,79 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import TodoHeader from './Header';
 import TodoList from './TodoList';
 import TodoInput from './TodoInput';
 import TodoFooter from './TodoFooter';
 import { View } from 'react-native';
 import { styles } from './css';
+import { ACTION_TYPE } from './Action';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPE.CHANGE_INPUT:
+      return { ...state, input: action.input };
+    case ACTION_TYPE.ADD_TODO:
+      return { ...state, todoItems: action.todoItems };
+    case ACTION_TYPE.CLEAR_INPUT:
+      return { ...state, input: {} };
+  }
+};
 
 const Main = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    input: {},
+    todoItems: [
+      {
+        id: Date.now(),
+        content: '할 일 예시',
+        isDone: false,
+        isEdit: false,
+      },
+      {
+        id: Date.now(),
+        content: '할 일 예시2',
+        isDone: true,
+        isEdit: false,
+      },
+      {
+        id: Date.now(),
+        content: '할 일 예시3',
+        isDone: false,
+        isEdit: true,
+      },
+    ],
+  });
+
+  const _changeInput = (key, value) => {
+    dispatch({
+      type: ACTION_TYPE.CHANGE_INPUT,
+      input: { ...state.input, [key]: value }
+    });
+  };
+
+  const changeTodoInput = (value) => {
+    _changeInput('todoInput', value);
+  };
+
+  const _createTodoItem = ({ content, id = Date.now(), isDone = false, isEdit = false }) => {
+    return { content, id, isDone, isEdit };
+  };
+
+  const addTodoItem = (content) => {
+    dispatch({
+      type: ACTION_TYPE.ADD_TODO,
+      todoItems: [...state.todoItems, _createTodoItem({ content })],
+    });
+    dispatch({ type: ACTION_TYPE.CLEAR_INPUT });
+  };
+
   return (
     <View style={styles.container}>
       <TodoHeader/>
-      <TodoInput/>
+      <TodoInput
+        inputValue={state.input.todoInput}
+        onChange={changeTodoInput}
+        onPress={addTodoItem}
+      />
       <TodoList/>
       <TodoFooter/>
     </View>
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,12 +3,17 @@ import { View, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from './css';
 import { AntDesign } from '@expo/vector-icons';
 
-const TodoInput = () => {
+const TodoInput = ({ inputValue, onChange, onPress }) => {
   return (
     <View style={styles.todoContainer}>
       <View style={styles.todoInput}>
-        <TextInput placeholder='Write your todo.' style={styles.fullFlex}/>
-        <TouchableOpacity onPress={() => console.log("touched!")}>
+        <TextInput
+          value={inputValue ? inputValue : ''}
+          onChangeText={onChange}
+          placeholder='Write your todo.'
+          style={styles.fullFlex}
+        />
+        <TouchableOpacity onPress={() => onPress(inputValue)}>
           <AntDesign name='pluscircle' size={18} color='#24a0ed'/>
         </TouchableOpacity>
       </View>
