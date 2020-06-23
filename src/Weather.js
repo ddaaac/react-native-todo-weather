2e4816ad703ab56685a102182923bc5f6dbcbd6f
BEOMJUN KIM <skyblue300a@gmail.com>
2020-06-23 17:55:23 +0900

    feat: 위치 정보를 받아와서 날씨 정보를 가져오는 기능 구현

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,66 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, View, Text, Image } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import useAxios from 'axios-hooks';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
-const Weather = ({ index }) => (
-  <View>
-    <Text>
-      This page number {index}!!!!
-    </Text>
-  </View>
-);
+import { URL } from './Api';
+import { styles } from './css';
+import TodoHeader from './Header';
+
+const appId = 'API_KEY';
+
+const Weather = ({ index }) => {
+  const [location, setLocation] = useState(null);
+
+  const [{ data: weatherData, loading: isWeatherLoading, error: weatherError }, getWeather] = useAxios(
+    URL.GET_WEATHER(location, appId),
+    { manual: true }
+  );
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await requestPermissionsAsync();
+      if (status === 'granted') {
+        const location = await getCurrentPositionAsync();
+        setLocation(location);
+      } else {
+        setLocation({ coords: { 'latitude': 37.7, 'longitude': 126.9 } });
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (location) {
+      getWeather();
+    }
+  }, [location]);
+
+  return (
+    <View style={styles.container}>
+      <TodoHeader title="Weather"/>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
+        {(!location || isWeatherLoading || !weatherData) && !weatherError ?
+          <View style={{ justifyContent: 'center', flex: 1 }}>
+            <ActivityIndicator size="large"/>
+          </View>
+          :
+          <View>
+            <View style={{ justifyContent: 'flex-end', alignItems: 'center', flex: 1 }}>
+              <Image style={{
+                width: 200,
+                height: 200,
+                resizeMode: 'stretch',
+              }} source={{ uri: URL.WEATHER_ICON(weatherData.weather[0].icon) }}/>
+            </View>
+            <Text style={{ flex: 1, fontSize: 30, textAlign: 'center' }}>
+              {weatherData.name}{'\n'}{Math.round(weatherData.main.temp * 10 - 2731.5) / 10}
+            </Text>
+          </View>
+        }
+      </View>
+    </View>
+  )
+};
 
 export default Weather;
