5f41f7b78dcc08a75c5a95bd0aa9f479cda2d7b6
BEOMJUN KIM <skyblue300a@gmail.com>
2020-06-24 12:09:07 +0900

    feat: 위치정보 동의 거부 시 Alert 기능 및 위치 정보 동의 설정 화면으로 이동시키는 기능 구현

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
-import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { View, Text, Alert, Platform, Linking } from 'react-native';
+import { requestPermissionsAsync, getCurrentPositionAsync, PermissionStatus } from 'expo-location';
+import { startActivityAsync, ACTION_LOCATION_SOURCE_SETTINGS } from 'expo-intent-launcher';
 
 import { styles } from './css';
 import TodoHeader from './Header';
@@ -9,11 +10,29 @@ import WeatherInfo from './WeatherInfo';
 const Weather = ({ index }) => {
   const [location, setLocation] = useState(null);
 
+  const openLocationSetting = async () => {
+    if (Platform.OS === 'android') {
+      // TODO: 안드로이드는 작동하는지 확인해봐야함
+      await startActivityAsync(ACTION_LOCATION_SOURCE_SETTINGS);
+    }
+    if (Platform.OS === 'ios') {
+      Linking.openURL('app-settings:');
+    }
+  };
+
   useEffect(() => {
     (async () => {
       const { status } = await requestPermissionsAsync();
-      if (status !== 'granted') {
-        // TODO: Alert
+      if (status !== PermissionStatus.GRANTED) {
+        Alert.alert(
+          '위치 정보 이용 동의',
+          '날씨 정보를 받아보고 싶다면 위치 정보 이용을 동의해주세요.',
+          [
+            { text: '지금 할게요', onPress: () => openLocationSetting() },
+            { text: '나중에 할게요' },
+          ],
+          { cancelable: false }
+        );
         return;
       }
       const location = await getCurrentPositionAsync();
